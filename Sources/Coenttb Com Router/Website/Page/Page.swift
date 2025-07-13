95d007dc209c078aec4878afb4cd736f0fd2b9f8
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-13 17:57:12 +0200

    fix broken links

--- Sources/Coenttb Com Router/Website/Page/Page.swift
+++ Sources/Coenttb Com Router/Website/Page/Page.swift
@@ -1,6 +1,6 @@
 //
 //  File 2.swift
-//  
+//
 //
 //  Created by Coen ten Thije Boonkkamp on 24-12-2023.
 //
@@ -31,46 +31,50 @@ public enum WebsitePage: Codable, Hashable, Sendable {
 
 extension WebsitePage {
     public struct Router: ParserPrinter, Sendable {
-
+        
         public init() {}
-
+        
         public var body: some URLRouting.Router<WebsitePage> {
             OneOf {
                 URLRouting.Route(.case(WebsitePage.account)) {
                     Path { String.account.slug() }
                     WebsitePage.Account.Router()
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.blog)) {
                     Path { String.blog.slug() }
                     Coenttb_Blog.Route.Router()
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.newsletter)) {
                     Path { String.newsletter.slug() }
                     Coenttb_Newsletter.View.Router()
                 }
-
+                
+                URLRouting.Route(.case(WebsitePage.newsletter(.subscribe(.request)))) {
+                    Path { String.newsletter.slug() + "/subscribe" }
+                }
+                
                 URLRouting.Route(.case(WebsitePage.choose_country_region)) {
                     Path { String.choose_country_region.slug() }
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.contact)) {
                     Path { String.contact.slug() }
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.privacy_statement)) {
                     Path { String.privacyStatement.slug() }
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.privacy_statement)) {
                     Path { String.privacyPolicy.slug() }
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.terms_of_use)) {
                     Path { String.terms_of_use.slug() }
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.general_terms_and_conditions)) {
                     Path { String.general_terms_and_conditions.slug() }
                 }
@@ -78,7 +82,7 @@ extension WebsitePage {
                 URLRouting.Route(.case(WebsitePage.identity)) {
                     Identity.View.Router()
                 }
-
+                
                 URLRouting.Route(.case(WebsitePage.home))
             }
         }
